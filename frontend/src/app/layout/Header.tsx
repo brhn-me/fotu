4cb524cf28dee45d3083ef66f9f20f9cecf9dd52
Burhan <burhan.bd@gmail.com>
2025-12-27 07:39:53 +0200

    feat: update project logo and favicon

--- frontend/src/app/layout/Header.tsx
+++ frontend/src/app/layout/Header.tsx
@@ -6,12 +6,7 @@ import { ThemeToggle } from "./ThemeToggle"; // Same folder imports
 import { ProfileMenu } from "./ProfileMenu";
 
 const PinwheelLogo = () => (
-    <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12 12C12 8.68629 9.31371 6 6 6C2.68629 6 0 8.68629 0 12C0 12 0 12 0 12H12Z" fill="#4285F4" transform="translate(12, 0)" />
-        <path d="M12 12C15.3137 12 18 9.31371 18 6C18 2.68629 15.3137 0 12 0C12 0 12 0 12 0V12Z" fill="#EA4335" transform="translate(0, 0)" />
-        <path d="M12 12C12 15.3137 14.6863 18 18 18C21.3137 18 24 15.3137 24 12C24 12 24 12 24 12H12Z" fill="#FBBC05" transform="translate(-12, 0)" />
-        <path d="M12 12C8.68629 12 6 14.6863 6 18C6 21.3137 8.68629 24 12 24C12 24 12 24 12 24V12Z" fill="#34A853" transform="translate(0, -12)" />
-    </svg>
+    <img src="/logo.png" alt="Fotu Logo" width="28" height="28" style={{ display: "block" }} />
 );
 
 const HeaderAction = ({ icon, label }: { icon: React.ReactNode; label: string }) => (
